9feeab655348b70c0a710c4b9c3293ca0fbc4032
Ariq Heritsa <ariqhm@gmail.com>
2022-11-21 18:47:12 +0700

    seat class

--- src/com/datscie/apponlineticketing/model/Seat.java
+++ src/com/datscie/apponlineticketing/model/Seat.java
@@ -1,5 +1,17 @@
 package com.datscie.apponlineticketing.model;
 
 public class Seat {
-    
+    private String seatID;
+
+    public Seat(String seatID) {
+        this.seatID = seatID;
+    }
+
+    public String getSeatID() {
+        return seatID;
+    }
+
+    public void setSeatID(String seatID) {
+        this.seatID = seatID;
+    }
 }
