b41e48c68c99b098b5c6152f4de0486cf2031c75
Ariq Heritsa <ariqhm@gmail.com>
2022-11-22 10:26:30 +0700

    admin and user

--- src/com/datscie/apponlineticketing/model/auth/Admin.java
+++ src/com/datscie/apponlineticketing/model/auth/Admin.java
@@ -1,5 +1,50 @@
 package com.datscie.apponlineticketing.model.auth;
 
-public class Admin {
-    
+import com.datscie.apponlineticketing.model.Movie;
+import com.datscie.apponlineticketing.model.Schedule;
+import com.datscie.apponlineticketing.model.Seat;
+import com.datscie.apponlineticketing.model.Studio;
+
+public class Admin extends User {
+    public void addMovie() {}
+
+    public void editMovie(String movieID) {}
+
+    public void deleteMovie(String movieID) {}
+
+    public Movie[] getMovies() {
+        return new Movie[]{};
+    }
+
+    public void addSchedule() {}
+
+    public void editSchedule(String scheduleID) {}
+
+    public void deleteSchedule(String scheduleID) {}
+
+    public Schedule[] getSchedules() {
+        return new Schedule[]{};
+    }
+
+    public void addStudio() {}
+
+    public void editStudio(String studioID) {}
+
+    public void deleteStudio(String studioID) {}
+
+    public Studio[] getStudios() {
+        return new Studio[]{};
+    }
+
+    public void addSeat() {}
+
+    public void editSeat(String seatID) {}
+
+    public void deleteSeat(String seatID) {}
+
+    public Seat[] getSeats() {
+        return new Seat[]{};
+    }
+
+    public void viewReports() {}
 }
--- src/com/datscie/apponlineticketing/model/auth/User.java
+++ src/com/datscie/apponlineticketing/model/auth/User.java
@@ -1,5 +1,21 @@
 package com.datscie.apponlineticketing.model.auth;
 
+import com.datscie.apponlineticketing.model.Schedule;
+import com.datscie.apponlineticketing.model.Seat;
+import com.datscie.apponlineticketing.model.Ticket;
+
 public class User extends Auth {
-    private 
+    private Ticket[] userTickets;
+
+    public Ticket[] getUserTickets() {
+        return userTickets;
+    }
+
+    public Schedule[] getAvailableSchedules() {
+        return new Schedule[]{};
+    }
+
+    public void buyTicket(Schedule schedule, Seat seat) {
+        
+    }
 }
